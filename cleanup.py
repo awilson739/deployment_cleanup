da96f168d0653c5d1c6db689c2d9df0b50a79237
Angela <angela.wilson739@gmail.com>
2024-02-12 18:34:43 -0500

    Add files via upload

---
+++ cleanup.py
@@ -0,0 +1,19 @@
+import boto3,click
+session = boto3.Session(profile_name='localstack')
+s3 = session.resource('s3')
+@click.command()
+@click.option('--deployments',help="Number of deployments to keep")
+@click.option('--bucket', help="S3 Deployment bucket")
+@click.option('--local',help="Sets the aws session profile name")
+
+def deployment_cleanup(deployments,bucket):
+    #get list of objects
+    deployment_objects = s3.Bucket(bucket).objects.all()
+    #sort objects by last modified
+    deletion_list = sorted(deployment_objects, key=lambda key:key.last_modified, reverse=True)[int(deployments):]
+    for deployment in deletion_list:
+        deployment.delete()
+
+
+if __name__ == '__main__':
+    deployment_cleanup()
